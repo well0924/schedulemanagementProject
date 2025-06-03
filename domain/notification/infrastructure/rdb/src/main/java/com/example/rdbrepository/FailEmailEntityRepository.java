e6539bd2c679ea810e076ed1ed273d9b7baa3168
well0924 <89343159+well0924@users.noreply.github.com>
2025-06-03 15:49:38 +0900

    - refactor : 이메일 재처리 수정

--- domain/notification/infrastructure/rdb/src/main/java/com/example/rdbrepository/FailEmailEntityRepository.java
+++ domain/notification/infrastructure/rdb/src/main/java/com/example/rdbrepository/FailEmailEntityRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface FailEmailEntityRepository extends JpaRepository<FailEmailEntity,Long> {
 
     List<FailEmailEntity> findByResolvedFalse();
-    void saveAll(List<FailEmailEntity> failEmails);
 }

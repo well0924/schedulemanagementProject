5c86479d075ef57b406d956f42adfceed3ff6ecf
well0924 <89343159+well0924@users.noreply.github.com>
2025-06-24 00:37:30 +0900

    - chore: gradle 충돌 수정

--- gradlew.bat
+++ gradlew.bat
@@ -13,11 +13,8 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
-<<<<<<< HEAD
-=======
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
->>>>>>> a4f2c18 (feat: S3 Presigned URL 업로드 및 Attach 등록 기능 구현)
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
@@ -48,19 +45,11 @@ set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
-<<<<<<< HEAD
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-=======
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
->>>>>>> a4f2c18 (feat: S3 Presigned URL 업로드 및 Attach 등록 기능 구현)
 
 goto fail
 
@@ -70,19 +59,11 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
-<<<<<<< HEAD
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-=======
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
->>>>>>> a4f2c18 (feat: S3 Presigned URL 업로드 및 Attach 등록 기능 구현)
 
 goto fail
 

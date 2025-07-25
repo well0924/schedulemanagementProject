b88c545f3e658b147a44b65e860971c8f2eb6526
well0924 <89343159+well0924@users.noreply.github.com>
2025-07-25 22:30:42 +0900

    - fix : OpenFeign에서 WebClient로 전환

--- domain/schedules/connector/out-bound/open-ai/src/main/java/com/example/outbound/openai/fallback/OpenAiFallback.java
+++ domain/schedules/connector/out-bound/open-ai/src/main/java/com/example/outbound/openai/fallback/OpenAiFallback.java
@@ -1,32 +1,25 @@
 package com.example.outbound.openai.fallback;
 
-import com.example.outbound.openai.client.OpenAiClient;
 import com.example.outbound.openai.dto.OpenAiRequest;
 import com.example.outbound.openai.dto.OpenAiResponse;
 
 import java.util.List;
 
-public class OpenAiFallback implements OpenAiClient {
+public class OpenAiFallback  {
 
-    @Override
-    public OpenAiResponse getChatCompletion(OpenAiRequest request) {
-        OpenAiRequest.Message fallbackMsg = OpenAiRequest
-                .Message
-                .builder()
+    public static OpenAiResponse fallbackResponse() {
+        OpenAiRequest.Message fallbackMsg = OpenAiRequest.Message.builder()
                 .role("assistant")
                 .content("지금은 추천을 제공할 수 없습니다.")
                 .build();
 
-        OpenAiResponse.Choice fallbackChoice = OpenAiResponse
-                .Choice
-                .builder()
+        OpenAiResponse.Choice fallbackChoice = OpenAiResponse.Choice.builder()
                 .index(0)
                 .message(fallbackMsg)
                 .finish_reason("fallback")
                 .build();
 
-        return OpenAiResponse
-                .builder()
+        return OpenAiResponse.builder()
                 .choices(List.of(fallbackChoice))
                 .build();
     }

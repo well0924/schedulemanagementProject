194401b5ad45fbb780932b3e5a54f24f9ab86314
well0924 <89343159+well0924@users.noreply.github.com>
2025-08-08 17:54:50 +0900

    - feat: 일정 추천 고도화
    - webclient 전환 (OpenFeign 라이브러리관련 수정)

--- bootstrap/src/main/java/com/example/BootStrapApplication.java
+++ bootstrap/src/main/java/com/example/BootStrapApplication.java
@@ -4,12 +4,10 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableFeignClients
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 @EntityScan(basePackages = "com.example")
